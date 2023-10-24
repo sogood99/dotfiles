4d9293819f36ab2657b13d69eb094d1dc20acdbd
Onii Chan <46487694+sogood99@users.noreply.github.com>
2023-10-24 01:26:31 -0400

    update treesitter keys

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -73,8 +73,8 @@ require 'nvim-treesitter.configs'.setup {
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = { query = "@class.outer", desc = "Next class start" },
+                ["]f"] = "@function.outer",
+                ["]c"] = { query = "@class.outer", desc = "Next class start" },
                 --
                 -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
                 ["]o"] = "@loop.*",
@@ -86,25 +86,25 @@ require 'nvim-treesitter.configs'.setup {
                 ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
             },
             goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
+                ["]F"] = "@function.outer",
+                ["]C"] = "@class.outer",
             },
             goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
+                ["[f"] = "@function.outer",
+                ["[c"] = "@class.outer",
             },
             goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
+                ["[F"] = "@function.outer",
+                ["[C"] = "@class.outer",
             },
             -- Below will go to either the start or the end, whichever is closer.
             -- Use if you want more granular movements
             -- Make it even more gradual by adding multiple queries and regex.
             goto_next = {
-                ["]f"] = "@conditional.outer",
+                ["]m"] = "@conditional.outer",
             },
             goto_previous = {
-                ["[f"] = "@conditional.outer",
+                ["[m"] = "@conditional.outer",
             }
         },
         lsp_interop = {
