c412170e6943e60760a7d19cd2e29614194da58f
Onii Chan <46487694+sogood99@users.noreply.github.com>
2024-06-18 20:39:45 -0400

    Update so mouseless compiles in install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,15 @@ config_dir=$HOME/.config
 dotfiles_dir=$config_dir/dotfiles
 current_dir=$(pwd)
 
-ln -s $current_dir $dotfiles_dir
+ln -sfn $current_dir $dotfiles_dir
+
+# mouseless compile
+cd mouseless/repo
+if ! command go build . &> /dev/null
+then
+    echo "Please install GO"
+    exit 1
+fi
+mv mouseless ../
+cd ../../
+
